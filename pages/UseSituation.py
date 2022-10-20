fc3143109353ae06c2f04454270dd4e39f81c365
akihiro.nakamura@denso-ten.com <akihiro.nakamura@denso-ten.com>
2022-10-20 20:18:14 +0900

    csvのソート機能&運転日報の追加

--- pages/UseSituation.py
+++ pages/UseSituation.py
@@ -13,6 +13,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import datetime
 from PIL import Image
 
 # タイトルの記載
@@ -35,30 +36,31 @@ def purpose():
 # 走行情報の関数
 def root():
     image = Image.open("./img/root.png")
-    st.image(image, caption="※DREC搭載時の拡張機能になります")
+    st.image(image, caption="※ドライブレコーダ搭載時の拡張機能になります")
 
 
 # 違反情報の関数
 def violation():
     image = Image.open("./img/violation.png")
-    st.image(image, caption="※DREC搭載時の拡張機能になります")
+    st.image(image, caption="※ドライブレコーダ搭載時の拡張機能になります")
 
 
 # カレンダー選択の関数。（対象の日付範囲に合わせて出力結果を出す）
 def calendar(start_date, end_date):
 
     # データの読み込み
-    df = pd.read_csv("./csv/Sample.csv", index_col=None, encoding="shift-jis")
+    df = pd.read_csv("./csv/利用状況.csv", index_col=None, encoding="shift-jis")
 
-    # Datetime型に統一
+    # Datetime型に統一（end_dateは当日も含める為、1日加算）
     df["available_date"] = pd.to_datetime(df["available_date"], format="%Y/%m/%d")
     start_date = pd.to_datetime(start_date, format="%Y/%m/%d")
-    end_date = pd.to_datetime(end_date, format="%Y/%m/%d")
+    end_date = pd.to_datetime(end_date, format="%Y/%m/%d") + datetime.timedelta(days=1)
 
     # 日付範囲の絞り込み
     df = df[df["available_date"] >= start_date]
     df = df[df["available_date"] <= end_date]
-    st.sidebar.dataframe(df, 600, 500)
+    st.sidebar.dataframe(df, 600, 400)
+    del df
 
 
 # 各種関数処理の出力
@@ -71,7 +73,7 @@ end_date = col[1].date_input("検索終了日")
 if start_date or end_date:
     calendar(start_date, end_date)
 
-# ボタンの横並び表示用準備
+# ボタン配置（横並び）
 col = st.columns(3)
 
 if col[0].button("目的地情報"):
